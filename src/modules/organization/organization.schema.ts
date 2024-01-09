dbf8754548b76e7868a94386a06df7a6a952a6a0
Muhammad Hidayatullah <dayatwork095@gmail.com>
2024-01-09 20:44:15 +0700

    fix: create organization

--- src/modules/organization/organization.schema.ts
+++ src/modules/organization/organization.schema.ts
@@ -1,15 +1,15 @@
-import { OrganizationType, Role } from "@prisma/client";
+import { OrganizationType } from "@prisma/client";
 import { z } from "zod";
 
 export const createOrganizationSchema = z.object({
   body: z.object({
     name: z.string({ required_error: "Name is required" }),
     type: z.nativeEnum(OrganizationType),
-    logo: z.string(),
-    province: z.string(),
-    city: z.string(),
-    district: z.string(),
-    subdistrict: z.string(),
+    logo: z.string().optional(),
+    province: z.string().optional(),
+    city: z.string().optional(),
+    district: z.string().optional(),
+    subdistrict: z.string().optional(),
   }),
 });
 
